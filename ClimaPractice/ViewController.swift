71e9c1646b06e531e12ecf4bb963a2404c6dfce2
jeff-0324 <jeffap324@gmail.com>
2023-09-08 11:36:11 +0900

    Initial Commit

---
+++ ClimaPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClimaPractice
+//
+//  Created by jae hoon lee on 2023/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
